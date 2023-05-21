524cdfea4d268379c43465d30b15d6ec0db79173
Christian <criszdev@gmail.com>
2023-05-21 16:56:53 -0300

    macaco

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import './App.css'
 
 function App() {
 
-  const monkeysList = [{id: 1, name: "Macaco Dentuço", src: "/macaco1.jpg"},
-                       {id: 2, name: "Mico leao dourado", src: "/macaco2.jpg"},
-                       {id: 3, name: "Orangotango", src: "/macaco3.jpg"}]
+  const monkeysList = [{id: 0, name: "Macaco Dentuço", src: "/macaco1.jpg"},
+                       {id: 1, name: "Mico leao dourado", src: "/macaco2.jpg"},
+                       {id: 2, name: "Orangotango", src: "/macaco3.jpg"}]
 
   const [monkey, setMonkey] = useState(monkeysList[0])
 
   const nextMonkey = () => {
     
     setMonkey((prev) => {
-      if (prev.id == 3) {
+      if (prev.id == 2) {
         return monkeysList[0]
       } else {
-        return monkeysList[prev.id]
+        return monkeysList[prev.id+1]
       }
     })
 
@@ -24,10 +24,10 @@ function App() {
   const backMonkey = () => {
 
     setMonkey((prev) => {
-      if (prev.id == 1) {
+      if (prev.id == 0) {
         return monkeysList[monkeysList.length-1]
       } else {
-        return monkeysList[prev.id-2]
+        return monkeysList[prev.id-1]
       }
     })
 
@@ -39,15 +39,15 @@ function App() {
       <h1>Macacos</h1>
 
       <div className='container'>
-        <img src="/icons8-back-arrow-50.png" className='arrow' onClick={backMonkey}/>
+        <img src="/back_arrow.png" className='arrow' onClick={backMonkey}/>
 
         <div>
-          <p>{monkey.id}/3</p>
+          <p>{monkey.id+1}/3</p>
           <h3>{monkey.name}</h3>
           <img src={monkey.src} className='monkey'/>
         </div>
         
-        <img src="/icons8-next-page-50.png" className='arrow' onClick={nextMonkey}/>
+        <img src="/next_arrow.png" className='arrow' onClick={nextMonkey}/>
       </div>
     </>
   )
